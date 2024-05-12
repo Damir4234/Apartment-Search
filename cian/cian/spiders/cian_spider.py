3d6243a30169511474f7311c70e9f89a867042fd
damir <saskedir@mail.ru>
2024-05-12 22:57:45 +0300

    code 200

--- cian/cian/spiders/cian_spider.py
+++ cian/cian/spiders/cian_spider.py
@@ -4,7 +4,8 @@ import scrapy
 class CianSpiderSpider(scrapy.Spider):
     name = "cian_spider"
     allowed_domains = ["kazan.cian.ru"]
-    start_urls = ["https://kazan.cian.ru"]
+    start_urls = ["https://kazan.cian.ru/sale/flat/301723007/"]
 
     def parse(self, response):
-        pass
+        title = response.css('.a10a3f92e9--title--vlZwT::text').get()
+        print(title)
